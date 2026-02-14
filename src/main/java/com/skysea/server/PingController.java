8a3cdaa2e5eaa8416dcd5d0cfe59c0bdf93ab87e
Melisa Martinez <melisamartinez201915@gmail.com>
2026-02-14 10:44:50 -0300

    Primer commit - estructura inicial del proyecto

---
+++ src/main/java/com/skysea/server/PingController.java
@@ -0,0 +1,13 @@
+package com.skysea.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Servidor funcionando";
+    }
+}
---
+++ src/main/java/com/skysea/server/logica/model/jugador.java
@@ -0,0 +1,5 @@
+package com.skysea.server.logica.model;
+
+public class jugador {
+    
+}
